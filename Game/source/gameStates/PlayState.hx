901a04281cef59af610eb2624024445a73cc03bc
Nicole Tan <nat34@cornell.edu>
2017-09-08 07:03:39 -0400

    Drawing boxes on screen upon click

--- Game/source/gameStates/PlayState.hx
+++ Game/source/gameStates/PlayState.hx
@@ -2,10 +2,16 @@ package gameStates;
 
 import flixel.FlxState;
 import flixel.FlxSprite; 
+import flixel.util.FlxColor; 
+import flixel.FlxG; 
+using flixel.util.FlxSpriteUtil; 
+
 
 class PlayState extends FlxState
 {
 	var player: FlxSprite = new FlxSprite(10, 10);
+	var canvas = new FlxSprite(); 
+
 
 	override public function create():Void
 	{
@@ -16,11 +22,27 @@ class PlayState extends FlxState
 		add(player); 
 		player.loadGraphic(AssetPaths.player__png, true, 16, 16);
 		player.animation.add("walk", [0, 1, 2, 3, 4, 5], 5, true);
+
+		canvas.makeGraphic(FlxG.width, FlxG.height, FlxColor.TRANSPARENT, true); 
+		add(canvas);
+
+		var lineStyle:LineStyle = {color: FlxColor.BLACK, thickness: 1}; 
+		var drawStyle:DrawStyle = {smoothing: true}; 
+		
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
 		player.animation.play("walk");
+
+		if (FlxG.mouse.justReleased) {
+			var box = new FlxSprite(FlxG.mouse.x, FlxG.mouse.y);
+			box.makeGraphic(32, 32, FlxColor.GREEN, true);
+			add(box);
+			FlxSpriteUtil.drawRoundRect(box, FlxG.mouse.x, FlxG.mouse.y, 32, 32, 5, 5, FlxColor.GREEN);
+
+			// canvas.drawRect(10, 10, FlxG.mouse.x, FlxG.mouse.y, FlxColor.BLUE); 
+		}
 	}
 }
