5f7149fce37889e9d00b457c1a23844a842d0cb4
Houka <cl795@cornell.edu>
2017-09-08 18:12:51 -0400

    improve run script and add pycache git ignore

--- Game/run.py
+++ Game/run.py
@@ -2,10 +2,16 @@ import os
 import sys
 
 if __name__ == "__main__":
-	build_language = "html5"
+	build_language = "neko"
 	try:
 		if len(sys.argv) > 1:
 			build_language = sys.argv[1]
+
+		# relocate to the directory of this file then run cmd
+		abspath = os.path.abspath(__file__)
+		dname = os.path.dirname(abspath)
+		os.chdir(dname)
+		print(os.getcwd())
 		os.system("lime test "+build_language)
 	except Error:
 		print("could NOT build and run with "+build_language)
