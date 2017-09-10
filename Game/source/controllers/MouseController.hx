795a4f9983dca1977a0b21b217b233077e0ea240
Nicole Tan <nat34@cornell.edu>
2017-09-10 17:36:29 -0400

    Create smoother dragging movement [but still not good]

--- Game/source/controllers/MouseController.hx
+++ Game/source/controllers/MouseController.hx
@@ -12,57 +12,44 @@ using flixel.util.FlxSpriteUtil;
 import controllers.*; 
 
 /**
- * The KeyboardController maps key presses to various flags for other
- * controllers to read in sync with the update step.
+ * The MouseController 
  */
 
 class MouseController extends FlxState
 {
-    // static private var _quit:Bool;
-    // static private var _paused:Bool;
 
     var spriteList:Array<FlxSprite> = new Array<FlxSprite>();  
     var width: Int = 32;
     var height: Int = 32; 
 
-    // public function new()
-    // {
-    //     super();
-    //     _quit = false;
-    //     _paused = false;
-    // }
+    override public function create(): Void { 
+        super.create(); 
+    }
 
     override public function update(elapsed:Float):Void
     {
         super.update(elapsed);
-        isOverlap(); 
-        mouseReleased(); 
-    }
-
-    private function mouseReleased(): Void { 
+        //if mouse is released and there's no object there
         if (FlxG.mouse.justReleased) {
-            var point: FlxPoint = new FlxPoint(FlxG.mouse.x, FlxG.mouse.y);
-            var box = new FlxSprite(FlxG.mouse.x, FlxG.mouse.y);
+            //Creates sprite 
+            var box = new FlxSprite(FlxG.mouse.x, FlxG.mouse.y); 
             box.makeGraphic(width, height, FlxColor.GREEN, true);
             add(box);
             spriteList.push(box);
-            // FlxMouseEventManager.add(box,isDragged);
-            // pointList.push(point);
-            // trace(pointList);
-            FlxSpriteUtil.drawRoundRect(box, FlxG.mouse.x, FlxG.mouse.y, width, height, 5, 5, FlxColor.GREEN);
-        }
-    }
 
-    private function isOverlap(): Void { 
-        for (i in 0...(spriteList.length-1)) {
-            //don't trust this overlaps function
-            if (FlxG.mouse.overlaps(spriteList[i])) {
-                if(FlxG.mouse.pressed){
-                    spriteList[i].setPosition(FlxG.mouse.getPosition().x, 
-                        FlxG.mouse.getPosition().y); 
+        }
+
+        //if mouse is pressed and object is at mouse location 
+        if (FlxG.mouse.pressed) {
+            trace("mouse is pressed"); 
+            //go through sprite list and see if there is a sprite at this location
+            for (i in 0...(spriteList.length - 1)) { 
+                var point:FlxPoint = new FlxPoint(FlxG.mouse.x, FlxG.mouse.y);
+                if (spriteList[i].overlapsPoint(point)) { 
+                    spriteList[i].setPosition(FlxG.mouse.x, FlxG.mouse.y);
+                    trace("there is overlap");
                 }
             }
         }
     }
-
 }
