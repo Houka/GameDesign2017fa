c42c1d7a737d3f327962b3626ce998913bc414dc
liyiming-serif <liyiming.serif@gmail.com>
2017-10-22 20:12:23 -0400

    first pass on logging

--- MTD/source/Logging.hx
+++ MTD/source/Logging.hx
@@ -2,12 +2,12 @@ package;
 
 extern class Logging {
     function new() : Void;
-    function initialize(gameId : UInt, versionId : UInt, debugMode : Bool) : Void;
+    function initialize(gameId : UInt, versionId : UInt, debugMode : Bool, suppressConsoleOutput : Bool) : Void;
     function assignABTestValue(candidate : UInt) : UInt;
     function recordABTestValue() : Void;
     function recordEvent(actionId : UInt, ?actionDetail : String) : Void;
     function recordLevelEnd() : Void;
     function recordLevelStart(questId : Float, ?questDetail : String) : Void;
     function recordPageLoad(?userInfo : String) : Void;
-    static function getSingleton() : Logging;
+    static function getLogging() : Logging;
 }
