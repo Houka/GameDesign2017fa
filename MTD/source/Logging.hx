6b6b9e7ac513dc2bb2d7437b9f12671f0e6cee49
liyiming-serif <liyiming.serif@gmail.com>
2017-10-23 22:35:12 -0400

    implement logging interface

--- MTD/source/Logging.hx
+++ MTD/source/Logging.hx
@@ -1,13 +1,32 @@
 package;
 
-extern class Logging {
-    function new() : Void;
-    function initialize(gameId : UInt, versionId : UInt, debugMode : Bool, suppressConsoleOutput : Bool) : Void;
-    function assignABTestValue(candidate : UInt) : UInt;
-    function recordABTestValue() : Void;
-    function recordEvent(actionId : UInt, ?actionDetail : String) : Void;
-    function recordLevelEnd() : Void;
-    function recordLevelStart(questId : Float, ?questDetail : String) : Void;
-    function recordPageLoad(?userInfo : String) : Void;
-    static function getLogging() : Logging;
+class Logging {
+    public function new(){
+
+    }
+    static public function initialize(gameId : UInt, versionId : UInt, debugMode : Bool, suppressConsoleOutput : Bool) : Void{
+        #if html5
+        untyped getLogging().initialize(gameId,versionId,debugMode,suppressConsoleOutput);
+        #end
+    }
+    static public function recordEvent(actionId : UInt, ?actionDetail : String) : Void{
+        #if html5
+        untyped getLogging().recordEvent(actionId, actionDetail);
+        #end
+    }
+    static public function recordLevelEnd() : Void{
+        #if html5
+        untyped getLogging().recordLevelEnd();
+        #end
+    }
+    static public function recordLevelStart(questId : Float, ?questDetail : String) : Void{
+        #if html5
+        untyped getLogging().recordLevelStart(questId, questDetail);
+        #end
+    }
+    static public function recordPageLoad(?userInfo : String) : Void{
+        #if html5
+        untyped getLogging().recordPageLoad(userInfo);
+        #end
+    }
 }
--- MTD/source/Main.hx
+++ MTD/source/Main.hx
@@ -3,12 +3,19 @@ package;
 import flixel.FlxGame;
 import openfl.display.Sprite;
 import gameStates.MenuState;
+import Logging;
 
 class Main extends Sprite
 {
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, MenuState, 1,60,60,true,false));
+		addChild(new FlxGame(0, 0, MenuState, 2,60,60,true,false));
+
+        Logging.initialize(771, 2, false, true);
+        Logging.recordPageLoad();
+        Logging.recordLevelStart(1.0);
+        Logging.recordEvent(1, "Hello world");
+        Logging.recordLevelEnd();
 	}
 }
