44d472f27ae6092fcff089f56ce9885016b90e16
Nicole Tan <nat34@cornell.edu>
2017-10-31 22:25:55 -0400

    Add d3 framework

---
+++ analytics/heatmap.js
@@ -0,0 +1,8 @@
+function create_heatmap(data, svg) { 
+	var circle = svg.append("circle")
+                          .attr("cx", 30)
+                          .attr("cy", 30)
+                         .attr("r", 20)
+                         .attr("fill", "black");
+
+}
