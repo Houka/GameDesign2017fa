ce616aeae4947666c31bb95a80807e0abc5277fd
Nicole Tan <nat34@cornell.edu>
2017-11-30 22:59:09 -0500

    Add correct ordering for up to level 9

--- MTD/source/LevelData.hx
+++ MTD/source/LevelData.hx
@@ -113,24 +113,9 @@ class LevelData{
 		buttonTypes:[0,1,2,3],
 		buildLimit:2
 	}
-	
+
 	public static var level8:Level = {
 		mapFilepath:"assets/maps/level8.csv",
-		tilemap:"assets/tiles/auto_tilemap.png",
-		startHealth:5,
-		waves:[[0, 0, 0],
-				[0, 0, 0,0,0],
-				[0, 1, 0, 1, 0],
-				[0, 0, 0, 1, 1,1],
-				[1, 1, 1, 1, 1, 1, 2, 2],
-				[2, 2, 1, 2, 1, 1, 2],
-				[2,2,2,2,2,2,2]],
-		buttonTypes:[0, 1,2, 3,4],
-		buildLimit:2
-	}
-	
-	public static var level9:Level = {
-		mapFilepath:"assets/maps/level9.csv",
 		tilemap:"assets/tiles/auto_tilemap.png",
 		startHealth:2,
 		waves:[[0,0,0,1],
@@ -138,6 +123,19 @@ class LevelData{
 				[0,1,0,2,2,2],
 				[1,1,1,1,2,2]],
 		buttonTypes:[0,1,2,3],
+		buildLimit:2
+	}
+
+	public static var level9:Level = {
+		mapFilepath:"assets/maps/level9.csv",
+		tilemap:"assets/tiles/auto_tilemap.png",
+		startHealth:2,
+		waves:[
+				[0,0,1,1,1], 
+				[1,2,1,2], 
+				[2,0,1,2,1],
+				[1,1,1,2,2,2]],
+		buttonTypes:[1,2,3,4],
 		buildLimit:2
 	}
 	
@@ -233,15 +231,18 @@ class LevelData{
 	public static var level16:Level = {
 		mapFilepath:"assets/maps/level16.csv",
 		tilemap:"assets/tiles/auto_tilemap.png",
-		startHealth:2,
-		waves:[
-				[0,0,1,1,1], 
-				[1,2,1,2], 
-				[2,0,1,2,1],
-				[1,1,1,2,2,2]],
-		buttonTypes:[1,2,3,4],
+		startHealth:5,
+		waves:[[0, 0, 0],
+				[0, 0, 0,0,0],
+				[0, 1, 0, 1, 0],
+				[0, 0, 0, 1, 1,1],
+				[1, 1, 1, 1, 1, 1, 2, 2],
+				[2, 2, 1, 2, 1, 1, 2],
+				[2,2,2,2,2,2,2]],
+		buttonTypes:[0, 1,2, 3,4],
 		buildLimit:2
 	}
+
 
 
 	public static var levels = [level1, level2, level3, level4, level5, level6, level7, level8, level9, level10, level11, level12, level13, level14, level15, level16];
